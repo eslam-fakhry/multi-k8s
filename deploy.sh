3c93cf2c5fdc08cd6b71798516ef4d71bac6e25e
Eslam Fakhry <eslam@seriescode.com>
2022-11-28 03:38:12 +0200

    deploy script

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+export GIT_SHA=$(git rev-parse HEAD )
+docker build -t eslamfakhry/multi-client:latest -t eslamfakhry/multi-client:$GIT_SHA -f ./client/Dockerfile ./client
+docker build -t eslamfakhry/multi-server:latest -t eslamfakhry/multi-server:$GIT_SHA-f ./server/Dockerfile ./server
+docker build -t eslamfakhry/multi-worker:latest -t eslamfakhry/multi-worker:$GIT_SHA -f ./worker/Dockerfile ./worker
+
+docker push eslamfakhry/multi-client:latest
+docker push eslamfakhry/multi-client:$GIT_SHA
+docker push eslamfakhry/multi-server:latest
+docker push eslamfakhry/multi-server:$GIT_SHA
+docker push eslamfakhry/multi-worker:latest
+docker push eslamfakhry/multi-worker:$GIT_SHA
+
+kubectl apply -f ./k8s
+
+kubectl set image deployment/server-deploymnent server=eslamfakhry/multi-server:$GIT_SHA 
+kubectl set image deployment/client-deploymnent client=eslamfakhry/multi-client:$GIT_SHA
+kubectl set image deployment/worker-deploymnent worker=eslamfakhry/multi-worker:$GIT_SHA 
